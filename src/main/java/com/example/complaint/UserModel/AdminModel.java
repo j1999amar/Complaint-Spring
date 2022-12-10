b95b591d53f126a1e837477e60f2bb0637f09b18
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-10 20:32:56 +0530

    View All Complaints

---
+++ src/main/java/com/example/complaint/UserModel/AdminModel.java
@@ -0,0 +1,15 @@
+package com.example.complaint.UserModel;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class AdminModel {
+
+
+
+}
