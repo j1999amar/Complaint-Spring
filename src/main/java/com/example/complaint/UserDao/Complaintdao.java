8825c51557d5448f7f870561c9d79836481fd486
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-10 17:21:15 +0530

    View Complaint

--- src/main/java/com/example/complaint/UserDao/Complaintdao.java
+++ src/main/java/com/example/complaint/UserDao/Complaintdao.java
@@ -1,7 +1,13 @@
 package com.example.complaint.UserDao;
 
 import com.example.complaint.UserModel.ComplaintModel;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface Complaintdao extends CrudRepository<ComplaintModel,Integer> {
+    @Query(value = "SELECT  * FROM `complaints` WHERE `user_id`= :userId",nativeQuery = true)
+    List<ComplaintModel> ViewComplaint(@Param("userId") String userId);
 }
