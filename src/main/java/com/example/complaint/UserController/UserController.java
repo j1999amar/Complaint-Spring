3cb92ff30a4fa0cde3d3d3921c9d47d78e3dda99
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-10 12:56:21 +0530

    User Registration Controller

--- src/main/java/com/example/complaint/UserController/UserController.java
+++ src/main/java/com/example/complaint/UserController/UserController.java
@@ -1,4 +1,16 @@
 package com.example.complaint.UserController;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/userRegistration",consumes = "application/json",produces = "application/json")
+    String UserRegistration( ){
+        return "hi";
+
+    }
 }
