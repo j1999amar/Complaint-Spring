49c2508d8b9bfbd125116e59cba1e7518fd732d0
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-10 13:11:50 +0530

    User Model

---
+++ src/main/java/com/example/complaint/UserModel/UserModel.java
@@ -0,0 +1,109 @@
+package com.example.complaint.UserModel;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Users")
+public class UserModel {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String address;
+    private String date;
+    private String phone;
+    private String email;
+    private String userLogin;
+    private String password;
+    private String confirmPassword;
+
+    public UserModel() {
+    }
+
+    public UserModel(int id, String name, String address, String date, String phone, String email, String userLogin, String password, String confirmPassword) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.date = date;
+        this.phone = phone;
+        this.email = email;
+        this.userLogin = userLogin;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
